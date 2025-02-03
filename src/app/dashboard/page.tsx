2b10fd20c75a76e71ba0aaa0e0885703344f769e
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-04 01:12:58 +0530

    add dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import EmailTemplateList from "@/components/core/Dashboard/EmailTemplateList";
+import { Button } from "@/components/ui/button";
+import { useAppSelector } from "@/lib/hooks";
+import { PlusIcon } from "lucide-react";
+import React from "react";
+
+const Dashboard = () => {
+  const { user } = useAppSelector((state) => state.auth);
+  return (
+    <div>
+      <div className="p-10 md:px-28 lg:px-40 xl:px-56 mt-16">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-2xl">Hello, {user.firstName}</h2>
+          <Button>
+            <PlusIcon className="w-4 h-4 mr-2" />
+            Create new email template
+          </Button>
+        </div>
+        <EmailTemplateList />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
