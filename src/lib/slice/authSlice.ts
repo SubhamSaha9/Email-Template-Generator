94954ddc7b64be6f7018572d6c0982c5f679f11f
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-05 00:41:14 +0530

    add authslice

---
+++ src/lib/slice/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    user: {
+        email: "",
+        firstName: "",
+        lastName: "",
+        image: "",
+    },
+    token: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: initialState,
+    reducers: {
+        setUser(state, value) {
+            state.user = value.payload;
+        },
+        setToken(state, value) {
+            state.token = value.payload;
+        },
+    },
+});
+
+export const { setUser, setToken } = authSlice.actions;
+export default authSlice.reducer;
