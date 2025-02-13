7e2165fb8975634da8c4aa6373ebdac20eef91f9
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-13 19:42:13 +0530

    update localstorage

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,6 +3,7 @@ import { setToken, setUser } from "@/lib/slice/authSlice";
 import { AppStore, makeStore } from "@/lib/store";
 import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
+import Cookies from "js-cookie";
 
 export default function StoreProvider({
   children,
@@ -15,11 +16,17 @@ export default function StoreProvider({
   }
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const storedToken = localStorage.getItem("token");
-    if (storedUser && storedToken && storeRef.current) {
-      storeRef.current.dispatch(setUser(JSON.parse(storedUser)));
-      storeRef.current.dispatch(setToken(JSON.parse(storedToken)));
+    const token = Cookies.get("token");
+    if (token) {
+      const storedUser = localStorage.getItem("user");
+      const storedToken = localStorage.getItem("token");
+      if (storedUser && storedToken && storeRef.current) {
+        storeRef.current.dispatch(setUser(JSON.parse(storedUser)));
+        storeRef.current.dispatch(setToken(JSON.parse(storedToken)));
+      }
+    } else {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
     }
   }, []);
 
