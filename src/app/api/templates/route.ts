ed49e3ab24fc3d0373e5e416dd9bfae8233d225e
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-15 18:47:32 +0530

    add logout route

---
+++ src/app/api/templates/route.ts
@@ -0,0 +1,29 @@
+import Template from "@/models/templates";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest) {
+    try {
+        const {id} = await req.json();
+
+        if(!id){
+            return NextResponse.json({
+                message: "Please provide id",
+                success: false
+            }, {status: 400});
+        }
+        const mongoId = new mongoose.Types.ObjectId(id);
+        const allTemplates = await Template.find({createdBy: mongoId}).select("-createdBy");
+
+        return NextResponse.json({
+                message: "Templates fetched successfully",
+                success: true,
+                data: allTemplates
+            }, {status: 200});
+    } catch (error:any) {
+         return NextResponse.json(
+              { message: error.message, success: false },
+              { status: 500 }
+        );
+    }
+}
