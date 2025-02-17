0939f0338b772a8065fd7d6769b290a7fe07cbd0
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-18 00:09:10 +0530

    add authentication middleware

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
     <div>
       <div className="p-10 md:px-28 lg:px-40 xl:px-56 mt-16">
         <div className="flex justify-between items-center">
-          <h2 className="font-bold text-2xl">Hello, {user.firstName}</h2>
+          <h2 className="font-bold text-2xl">Hello, {user?.firstName}</h2>
           <Button onClick={() => router.push("/dashboard/create")}>
             <PlusIcon className="w-4 h-4 mr-2" />
             Create new email template
